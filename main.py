4c03e89977c233c3edb49631f2425abe8a6d5f92
svmadap <81061506+svmadap@users.noreply.github.com>
2021-06-20 00:56:39 -0400

    login final

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ past_ans = []
 def index():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 @app.route('/test', methods=["POST"])  # Happens when the form submits.
 def wolframRequest():
